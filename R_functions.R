d2983ca22a72492bd53bb54da570eae947eeafe3
Daniel Warner <37349736+Dan-Warner@users.noreply.github.com>
2023-03-24 15:13:22 -0400

    Update R_functions.R

--- R_functions.R
+++ R_functions.R
@@ -1,64 +1,29 @@
-# Arrange df vars by position ----
-# vars' must be a named vector, e.g. c("var.name"=1)
+## Cumulative frequency ----
+# does not work with adorn_totals() 
+adorn_cumulative <- function(dat, colname, dir = "down"){
 
-arrange.vars <- function(data, vars){
-  ##stop if not a data.frame (but should work for matrices as well)
-  stopifnot(is.data.frame(data))
-  
-  ##sort out inputs
-  data.nms <- names(data)
-  var.nr <- length(data.nms)
-  var.nms <- names(vars)
-  var.pos <- vars
-  ##sanity checks
-  stopifnot( !any(duplicated(var.nms)), 
-             !any(duplicated(var.pos)) )
-  stopifnot( is.character(var.nms), 
-             is.numeric(var.pos) )
-  stopifnot( all(var.nms %in% data.nms) )
-  stopifnot( all(var.pos > 0), 
-             all(var.pos <= var.nr) )
-  
-  ##prepare output
-  out.vec <- character(var.nr)
-  out.vec[var.pos] <- var.nms
-  out.vec[-var.pos] <- data.nms[ !(data.nms %in% var.nms) ]
-  stopifnot( length(out.vec)==var.nr )
-  
-  ##re-arrange vars by position
-  data <- data[ , out.vec]
-  return(data)
+  if(!missing(colname)){
+    colname <- rlang::enquo(colname)
+  } else if("valid_percent" %in% names(dat)) {
+  colname <- rlang::sym("valid_percent")
+  } else if("percent" %in% names(dat)){
+    colname <- rlang::sym("percent")
+  } else {
+    stop("\"colname\" not specified and default columns valid_percent and percent are not present in data.frame dat")
+  }
+
+  target <- dplyr::pull(dat, !! colname)
+
+  if(dir == "up"){
+    target <- rev(target)
+  }
+  dat$cumulative <- cumsum(ifelse(is.na(target), 0, target)) + target*0 # an na.rm version of cumsum, from https://stackoverflow.com/a/25576972
+  if(dir == "up"){
+    dat$cumulative <- rev(dat$cumulative)
+    names(dat)[names(dat) %in% "cumulative"] <- "cumulative_up"
+  }
+  dat
 }
-
-# arrange.vars(df, c("a" = 3))
-
-
-
-# Move specific column to last, first, or in between (before / after) position ----
-
-moveMe <- function(data, tomove, where = "last", ba = NULL) {
-  temp <- setdiff(names(data), tomove)
-  x <- switch(
-    where,
-    first = data[c(tomove, temp)],
-    last = data[c(temp, tomove)],
-    before = {
-      if (is.null(ba)) stop("must specify ba column")
-      if (length(ba) > 1) stop("ba must be a single character string")
-      data[append(temp, values = tomove, after = (match(ba, temp)-1))]
-    },
-    after = {
-      if (is.null(ba)) stop("must specify ba column")
-      if (length(ba) > 1) stop("ba must be a single character string")
-      data[append(temp, values = tomove, after = (match(ba, temp)))]
-    })
-  x
-}
-
-# moveMe(df, c("b", "c"))
-# moveMe(df, c("b", "c"), "first")
-# moveMe(df, c("b", "c"), "before", "e")
-# moveMe(df, c("b", "c"), "after", "e")
 
 
 
